3f8d8745b6f6aa719b17e86232eda253699cfa72
Gerd Bleher <gerd.bleher@advantest.com>
2024-05-07 13:25:06 +0200

    Some cleanup in the build script

--- ANTLR/build.sh
+++ ANTLR/build.sh
@@ -1,16 +1,29 @@
 #! /usr/bin/bash
 
+# set -x
+
 ANTLR=antlr-4.13.1-complete.jar
-SRCS='src/*.java'
 
 cd $(dirname $0)
-cd src
 
-if [[ ! ( -r PfxLexer.java && -r PfxParser.java && -r PfxVisitor.java && -r PfxBaseVisitor.java && PfxParser.java -nt Pfx.g4 ) ]]; then
+# Generate code from the grammar only if the grammar has changed
+cd src
+if [[ ! ( -r PfxLexer.java && -r PfxParser.java && -r PfxVisitor.java && -r PfxBaseVisitor.java && PfxParser.java -nt Pfx.g4 ) ]]
+then
     echo java -jar ../lib/$ANTLR -visitor -no-listener Pfx.g4
     java -jar ../lib/$ANTLR -visitor -no-listener Pfx.g4
 fi
-
 cd ..
-echo javac -cp dst\;lib/$ANTLR -d dst $SRCS
-javac -cp dst\;lib/$ANTLR -d dst $SRCS
+
+# Compile all Java files in src
+DIRS="src"
+if [[ -d src/util ]]; then DIRS="$DIRS src/util"; fi
+if [[ -d src/ast ]]; then DIRS="$DIRS src/ast"; fi
+
+SRCS=""
+for d in $DIRS; do SRCS="$SRCS $d/*.java"; done
+
+# CLASSPATH="dst:lib/$ANTLR"	
+CLASSPATH="dst\\;lib/$ANTLR"	# This is a peculiarity of running the script in the git bash
+echo javac -cp $CLASSPATH -d dst $SRCS
+javac -cp $CLASSPATH -d dst $SRCS
