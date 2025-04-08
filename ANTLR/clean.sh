1322782abdc2d56880480152d10eb25d811f7e65
Gerd Bleher <gerd.bleher@advantest.com>
2025-04-08 09:28:59 +0200

    Fix: clean up assemply code in test dir

--- ANTLR/clean.sh
+++ ANTLR/clean.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 cd $(dirname $0)
-rm -rf dst/* gen/*
+rm -rf dst/* gen/* tst/*.s
 
 cd src
 rm -f *.interp *.tokens PfxBaseVisitor.java PfxBaseListener.java PfxVisitor.java PfxListener.java PfxLexer.java PfxParser.java *.class
