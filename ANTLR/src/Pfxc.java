5394e3c277a75ca988dae6c1272bc65aa1980b33
Gerd Bleher <gerd.bleher@advantest.com>
2025-04-16 11:12:16 +0200

    Fix: Stop compilation after syntax error

--- ANTLR/src/Pfxc.java
+++ ANTLR/src/Pfxc.java
@@ -25,6 +25,9 @@ class Pfxc {
       PfxLexer lexer = new PfxLexer(CharStreams.fromFileName(this.inputFileName));
       PfxParser parser = new PfxParser(new CommonTokenStream(lexer));
       ParserRuleContext parseTree = parser.program();
+      if (parser.getNumberOfSyntaxErrors() > 0) {
+        throw new RuntimeException();
+      }
       AsmWriter asm = new AsmWriter(baseName() + ".s");
     }
     catch (IOException e) {
